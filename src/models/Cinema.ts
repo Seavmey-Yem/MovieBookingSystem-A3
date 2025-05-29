43e1c7d829887f003634c10f6986b1e8db2109e2
HORTH2025 <horth.heng@student.passerellesnumeriques.org>
2025-05-29 20:26:45 +0700

    create class cinema #5

---
+++ src/models/Cinema.ts
@@ -0,0 +1,9 @@
+// import { Showtime } from "./Showtime";
+export class Cinema {
+  constructor(
+    public id: number,
+    public name: string,
+    public location: string,
+    // public showtimes: Showtime[] = []
+  ) {}
+}
