eb48e3ac0fb09224926e9ed3a48cbf460c74337c
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-05-30 08:11:53 +0700

    feat(feature/ticket): added this connection with public attribute in class Ticket [gitboard-12]

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -1,8 +1,16 @@
 import { Booking } from "./Booking";
 
 export class Ticket {
-    public id: number;
-    public booking: Booking; // Composition: a Ticket "has a" Booking
-    public qrCode: string;
-    public referenceNumber: string;
+    constructor(
+            public id: number,
+            public booking: Booking,// Composition: a Ticket "has a" Booking
+            public qrCode: string,
+            public referenceNumber: string,
+    )  {
+        this.id = id;
+        this.booking = booking;
+        this.qrCode = qrCode;
+        this.referenceNumber = referenceNumber;
+    }
+
 }
