a7e4901d61b6190f2c5512462a43128d2d947025
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-05-30 08:18:29 +0700

    feat(feature/Ticket): added public ticket class on file Ticket [gitboard-12]

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -1,12 +1,17 @@
 import { Booking } from "./Booking";
 
 export class Ticket {
+ public id: number;
+    public booking: Booking;
+    public qrCode: string;
+    public referenceNumber: string;
+
     constructor(
-            public id: number,
-            public booking: Booking,// Composition: a Ticket "has a" Booking
-            public qrCode: string,
-            public referenceNumber: string,
-    )  {
+        id: number,
+        booking: Booking,
+        qrCode: string,
+        referenceNumber: string
+    ) {
         this.id = id;
         this.booking = booking;
         this.qrCode = qrCode;
