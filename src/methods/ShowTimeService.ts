3286561cfeddc8832ee4ce5729c525cfadbf81ae
HORTH2025 <horth.heng@student.passerellesnumeriques.org>
2025-05-31 13:01:54 +0700

    Create show time service #17

--- src/methods/ShowTimeService.ts
+++ src/methods/ShowTimeService.ts
@@ -1,10 +1,10 @@
 import { Seat } from "../models/Seat";
 import { ShowTime } from "../models/ShowTime";
+import { Status } from "../enums/Status";
+import { Movie } from "../models/Movie";
 
 export class ShowTimeService {
-    getAvailableSeats(showtime: ShowTime): Seat[] {
-        return ShowTime.Seat.filter((Seat: { Status: string; }) => Seat.Status);
-
-    }
-
+  getAvailableSeats(showtime: ShowTime): Seat[] {
+    return showtime.theater.seat.filter(seat => seat.status === Status.AVAILABLE);
+  }
 }
