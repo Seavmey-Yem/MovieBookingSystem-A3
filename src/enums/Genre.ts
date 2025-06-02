2e1eda8763d68c389216d741d56087df091ef1b7
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-06-02 16:32:31 +0700

    feat command line to make understand in each methods #8

--- src/enums/Genre.ts
+++ src/enums/Genre.ts
@@ -1,12 +1,13 @@
+// Genres for content classification
 export enum Genre {
-    HORROR = 'Horror',
-    FUNNY = 'Funny',
-    LOVESTORY = 'Lovestory',
-    DRAMA = 'Drama',
-    HISTORICAL = 'Historical',
-    ADVENTURE = 'Adventure',
-    CARTOON = 'Cartoon',
-    ANIME = 'Anime',
-    ACTION = 'Action',
-    WAR = 'War'
+    HORROR = "Horror",
+    FUNNY = "Funny",
+    LOVESTORY = "Lovestory",
+    DRAMA = "Drama",
+    HISTORICAL = "Historical",
+    ADVENTURE = "Adventure",
+    CARTOON = "Cartoon",
+    ANIME = "Anime",
+    ACTION = "Action",
+    WAR = "War"
 }
