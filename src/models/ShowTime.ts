bf3ef6fbebec0b0d959896f69c51c83c4262e90f
Seavmey-Yem <seavmey.yem@student.passerellesnumeriques.org>
2025-06-04 07:51:46 +0700

    Wrote comment code in ShowThime

--- src/models/ShowTime.ts
+++ src/models/ShowTime.ts
@@ -2,6 +2,7 @@ import { Cinema } from "./Cinema";
 import { Movie } from "./Movie";
 import { Theater } from "./Theater";
 
+// Represents a showtime for a movie at a specific cinema and theater.
 export class ShowTime {
     constructor(
         public id : number,
