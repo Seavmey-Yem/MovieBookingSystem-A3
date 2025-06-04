3e9689036e080750bbf08657c79a83e637710305
Seavmey-Yem <seavmey.yem@student.passerellesnumeriques.org>
2025-06-04 07:42:34 +0700

    Wrote comment method in ShowThimeService

--- src/methods/ShowTimeService.ts
+++ src/methods/ShowTimeService.ts
@@ -4,6 +4,8 @@ import { Status } from "../enums/Status";
 import { Movie } from "../models/Movie";
 
 export class ShowTimeService {
+
+  // Returns all available seats for the given showtime.
   getAvailableSeats(showtime: ShowTime): Seat[] {
     return showtime.theater.seat.filter(seat => seat.status === Status.AVAILABLE);
   }
